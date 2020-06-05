41d79141d2e1974f39e9894ea1979ebfe45b1ebf
RCristyan <rcristyan9@gmail.com>
2020-06-05 19:37:22 -0300

    Criado banco de dados.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const server = express()
 
+const db = require("./database/db.js")
+
 server.use(express.static("public"))
+server.use(express.urlencoded({ extended: true }))
 
 const nunjucks = require("nunjucks")
 nunjucks.configure("src/views", {
@@ -16,11 +19,61 @@ server.get("/", (requisition, response) => {
 })
 
 server.get("/create-point", (requisition, response) => {
+
+    console.log(requisition.query)
+
     return response.render("create-point.html")
 })
 
+server.post("/savepoint", (requisition, response) => {
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);`
+
+    const values = [
+        requisition.body.image,
+        requisition.body.name,
+        requisition.body.address,
+        requisition.body.adress2,
+        requisition.body.state,
+        requisition.body.city,
+        requisition.body.items
+    ]
+
+    function afterInsertData(err){
+        if(err){
+            return console.log(err)
+        }
+    
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return response.render("create-point.html", { saved: true })
+    }
+
+    db.run(query, values, afterInsertData)
+    
+})
+
 server.get("/search", (requisition, response) => {
-    return response.render("search-results.html")
+
+    db.all("SELECT * FROM places", function(err, rows) {
+        if(err){
+            return console.log(err)
+        }
+
+        const total = rows.length
+
+        return response.render("search-results.html", { places: rows, total})
+    })
+
 })
 
 server.listen(3000)
